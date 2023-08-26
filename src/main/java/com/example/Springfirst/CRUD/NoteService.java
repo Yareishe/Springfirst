fef8b210bf437a98415582c5dc76ce01f48c620f
Yareishe <121358447+Yareishe@users.noreply.github.com>
2023-08-26 03:40:36 +0300

    Create NoteService.java

---
+++ src/main/java/com/example/Springfirst/CRUD/NoteService.java
@@ -0,0 +1,11 @@
+package com.example.Springfirst.CRUD;
+
+import java.util.List;
+
+public interface NoteService {
+    List<Note> listAll();
+    Note add(Note note);
+    void deleteById(long id);
+    void update(Note note);
+    Note getById(long id);
+}
