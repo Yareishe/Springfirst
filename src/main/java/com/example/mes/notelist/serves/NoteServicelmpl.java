ce5ea105e608d180df7cdf3b3332561fe63e0293
Yareishe <121358447+Yareishe@users.noreply.github.com>
2023-09-12 03:20:55 +0300

    Create NoteServicelmpl.java

---
+++ src/main/java/com/example/mes/notelist/serves/NoteServicelmpl.java
@@ -0,0 +1,49 @@
+package com.example.mes.notelist.serves;
+
+
+import com.example.mes.notelist.entity.Note;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+@Service
+public class NoteServicelmpl implements NoteService {
+
+
+    public Map<Long, Note> notesMap = new HashMap<>();
+    public Long avtoid = 1L;
+
+
+    @Override
+    public List<Note> listAll() {
+
+        return new ArrayList<>(notesMap.values()) ;
+    }
+    @Override
+    public Note add(Note note) {
+        notesMap.put(note.getId(),note);
+        return note;
+    }
+
+    @Override
+    public void deleteById(long id) {
+        notesMap.remove(id);
+    }
+
+    @Override
+    public void update(Note note) {
+        notesMap.put(note.getId(),note);
+
+    }
+
+    @Override
+    public Note getById(long id) {
+        return notesMap.get(id);
+    }
+
+
+}
