c3eb0f6daa3ca5a2e2d43abaf750de23c90b3446
Yareishe <121358447+Yareishe@users.noreply.github.com>
2023-09-12 03:20:28 +0300

    Create MassageControler.java

---
+++ src/main/java/com/example/mes/notelist/controler/MassageControler.java
@@ -0,0 +1,74 @@
+package com.example.mes.notelist.controler;
+
+import com.example.mes.notelist.serves.NoteServicelmpl;
+import com.example.mes.notelist.entity.Note;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller()
+@RequestMapping("/note")
+@Slf4j
+public class MassageControler {
+
+
+    private NoteServicelmpl noteServicelmpl;
+    private Long id = 1L;
+
+    public MassageControler(NoteServicelmpl noteServicelmpl) {
+        this.noteServicelmpl = noteServicelmpl;
+    }
+
+
+    @GetMapping("/list")
+    public String listNotes(Model model) {
+        model.addAttribute("notes", noteServicelmpl.listAll());
+        return "list";
+    }
+
+
+
+    @PostMapping(value = "/delete")
+    public String deleteNotes(@RequestParam Long id) {
+        noteServicelmpl.deleteById(id);
+        return "redirect:/note/list";
+    }
+
+    @GetMapping("/add")
+    public String addgetNotes(@ModelAttribute("note") Note note, Model model) {
+        return "add";
+    }
+
+    @PostMapping("/add")
+    public String addpostNotes(@ModelAttribute("note") Note note, Model model) {
+        Note newNode = new Note(id,note.getTitle(),note.getContent());
+        noteServicelmpl.update(newNode);
+        id++;
+        return "redirect:/note/list";
+    }
+
+    @GetMapping("/edit")
+    public String editeNotes(@RequestParam("id") Long id, Model model) {
+        Note newNote1 = noteServicelmpl.getById(id);
+        model.addAttribute("note", newNote1);
+        return "edit";
+    }
+
+    @PostMapping("/edit")
+    public String editNotes(@ModelAttribute("note") Note note, Model model) {
+
+        Note newNote = noteServicelmpl.getById(note.getId());
+        newNote.setTitle(note.getTitle());
+        newNote.setContent(note.getContent());
+        noteServicelmpl.update(newNote);
+
+        model.addAttribute("note", newNote);
+        return "redirect:/note/list";
+    }
+
+
+
+
+}
