76abf68b2ae45dc3b9c8e724540890c4f367d146
Yareishe <121358447+Yareishe@users.noreply.github.com>
2023-09-17 02:15:22 +0300

    Update MesApplication.java

--- src/main/java/com/example/mes/MesApplication.java
+++ src/main/java/com/example/mes/MesApplication.java
@@ -6,9 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MesApplication {
 
-   
-
-    
     public static void main(String[] args) {
         SpringApplication.run(MesApplication.class, args);
     }
