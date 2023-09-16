b5a8ff5284ed302c9e5f28eee8a90e15bf2433e2
Yareishe <121358447+Yareishe@users.noreply.github.com>
2023-09-17 02:15:35 +0300

    Update NoteService.java

--- src/main/java/com/example/mes/notelist/serves/NoteService.java
+++ src/main/java/com/example/mes/notelist/serves/NoteService.java
@@ -4,11 +4,13 @@ package com.example.mes.notelist.serves;
 import com.example.mes.notelist.entity.Note;
 
 import java.util.List;
+import java.util.Optional;
+
 
 public interface NoteService {
     List<Note> listAll();
     Note add(Note note);
     void deleteById(long id);
     void update(Note note);
-    Note getById(long id);
+    Optional<Note> getById(long id);
 }
